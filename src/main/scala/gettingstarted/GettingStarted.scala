94efe8041fe348840b4df734c300190e6fdaf912
ldl <wsldl123292@163.com>
2016-06-06 22:26:14 +0800

    高阶函数

--- src/main/scala/gettingstarted/GettingStarted.scala
+++ src/main/scala/gettingstarted/GettingStarted.scala
@@ -15,6 +15,19 @@ object GettingStarted {
         loop(n, 0, 1)
     }
 
+
+    def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+        @annotation.tailrec
+        def go(n: Int): Boolean = {
+            if (n >= as.length - 1) true
+            else if (ordered(as(n), as(n + 1))) false
+            else go(n + 1)
+        }
+
+        go(0)
+
+    }
+
     def main(args: Array[String]) {
         println(fib(1))
     }
